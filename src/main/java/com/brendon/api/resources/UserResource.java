8fb49a7fc12f09507daf6ff6bc5c926d6eaa3ba0
brendongama <brendon_gama@hotmail.com>
2021-12-07 18:05:00 -0300

    interface User resource findById

---
+++ src/main/java/com/brendon/api/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.brendon.api.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brendon.api.domain.User;
+import com.brendon.api.services.UserService;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+	
+	@Autowired
+	private UserService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Integer id) {
+		return ResponseEntity.ok().body(service.findById(id));
+	}
+}
