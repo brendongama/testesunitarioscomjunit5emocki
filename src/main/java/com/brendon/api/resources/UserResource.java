667ffa160f5ceabe3acd12432e83bee98ce35ca1
brendongama <brendon_gama@hotmail.com>
2021-12-08 10:34:06 -0300

    EndPoint findAll para user

--- src/main/java/com/brendon/api/resources/UserResource.java
+++ src/main/java/com/brendon/api/resources/UserResource.java
@@ -1,5 +1,8 @@
 package com.brendon.api.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brendon.api.domain.User;
 import com.brendon.api.domain.dto.UserDTO;
 import com.brendon.api.services.UserService;
 
@@ -24,6 +28,14 @@ public class UserResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
 		return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> findAll(){		
+		return ResponseEntity.ok()
+				.body(service.findAll().stream()
+						.map(x -> mapper.map(x,  UserDTO.class))
+						.collect(Collectors.toList()));
 	}
 }
  
--- src/main/java/com/brendon/api/services/UserService.java
+++ src/main/java/com/brendon/api/services/UserService.java
@@ -1,8 +1,11 @@
 package com.brendon.api.services;
 
+import java.util.List;
+
 import com.brendon.api.domain.User;
 
 public interface UserService {
 
 	User findById(Integer id);
+	List<User> findAll();
 }
