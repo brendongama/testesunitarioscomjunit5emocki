9e44dff77ad32eb20561c5e49f5e629f1e6d0f4d
brendongama <brendon_gama@hotmail.com>
2021-12-08 22:16:26 -0300

    endPoint Create

--- src/main/java/com/brendon/api/services/UserService.java
+++ src/main/java/com/brendon/api/services/UserService.java
@@ -3,9 +3,11 @@ package com.brendon.api.services;
 import java.util.List;
 
 import com.brendon.api.domain.User;
+import com.brendon.api.domain.dto.UserDTO;
 
 public interface UserService {
 
 	User findById(Integer id);
 	List<User> findAll();
+	User create(UserDTO obj);
 }
--- src/main/java/com/brendon/api/services/impl/UserServiceImpl.java
+++ src/main/java/com/brendon/api/services/impl/UserServiceImpl.java
@@ -3,10 +3,12 @@ package com.brendon.api.services.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brendon.api.domain.User;
+import com.brendon.api.domain.dto.UserDTO;
 import com.brendon.api.repositories.UserRepository;
 import com.brendon.api.services.UserService;
 import com.brendon.api.services.exceptions.ObjectNotFoundException;
@@ -16,6 +18,9 @@ public class UserServiceImpl implements UserService{
 	
 	@Autowired
 	private UserRepository repository;
+	
+	@Autowired
+	private ModelMapper mapper;
 
 	@Override
 	public User findById(Integer id) {
@@ -25,6 +30,11 @@ public class UserServiceImpl implements UserService{
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	@Override
+	public User create(UserDTO obj) {
+		return repository.save(mapper.map(obj, User.class));
 	}
 
 }
